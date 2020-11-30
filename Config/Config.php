446702ce111963a3aabe536f24c531a2f33f05a6
Andy Paz <andyalexpaz@live.com>
2020-11-29 23:31:18 -0600

    Estructuraacion

---
+++ Config/Config.php
@@ -0,0 +1,12 @@
+<?php 
+	
+	const BASE_URL = "http://localhost/tienda_virtual/";
+
+	//Datos de conexion a base de datos
+
+	const DB_HOST = "localhost";
+	const DB_NAME = "store";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "charset=utf8";
+ ?>
